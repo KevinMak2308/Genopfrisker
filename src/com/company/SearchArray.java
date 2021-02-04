cbaed0caebe0be2592b4532126992f8bc0b7011e
KevinMak2308 <kevi4196@stud.kea.dk>
2021-02-04 18:28:28 +0100

    Opgave 4 og 7

---
+++ src/com/company/SearchArray.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+
+public class SearchArray {
+
+
+    public int searchArrayForString(String[] arr, String sd) {
+        if (arr.length == 0) {
+            return -1;
+
+        } else {
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].equals(sd)) {
+                    return i;
+                } else {
+                    return -1;
+                }
+
+            }
+
+        }
+
+    }
+}
+
---
+++ src/com/company/SquareStars.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class SquareStars {
+
+    public void theStarSquare() {
+
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        }
+    }
+
